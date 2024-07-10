5ec634ad73df37267ff809102135a0c6a41042de
Avik Kundu <avikkundu8d@gmail.com>
2024-07-10 09:24:19 +0530

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -10,13 +10,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore"
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyA2VDqJZ6Xh65TowUvPvjsOzyTBCQU8Ln4",
+  apiKey: "",
   authDomain: "oc-neutral.firebaseapp.com",
   projectId: "oc-neutral",
   storageBucket: "oc-neutral.appspot.com",
-  messagingSenderId: "592729192715",
-  appId: "1:592729192715:web:3a82f8fc0f106a7e740a0e",
-  measurementId: "G-KCZ02PVRSG",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
